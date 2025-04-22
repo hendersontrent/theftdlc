fca0c6e053a974fba0766053d8910c091a855eb2
Trent Henderson <trent.henderson1@outlook.com>
2025-04-23 09:12:26 +1000

    boxplot

--- R/plots.R
+++ R/plots.R
@@ -11,7 +11,7 @@
 #' @importFrom stats hclust dist cor
 #' @importFrom normaliseR normalise
 #' @param x \code{feature_calculations} object containing the raw feature matrix produced by \code{theft::calculate_features}
-#' @param type \code{character} specifying the type of plot to draw. Defaults to \code{"quality"}
+#' @param type \code{character} specifying the type of plot to draw. Can be one of \code{"matrix"}, \code{"cor"}, \code{"violin"}, \code{"box"}, or \code{"quality"}. Defaults to \code{"matrix"}
 #' @param norm_method \code{character} specifying a rescaling/normalising method to apply if \code{type = "matrix"} or if \code{type = "cor"}. Can be one of \code{"zScore"}, \code{"Sigmoid"}, \code{"RobustSigmoid"}, \code{"MinMax"}, or \code{"MaxAbs"}. Defaults to \code{"zScore"}
 #' @param unit_int \code{Boolean} whether to rescale into unit interval \code{[0,1]} after applying normalisation method. Defaults to \code{FALSE}
 #' @param clust_method \code{character} specifying the hierarchical clustering method to use if \code{type = "matrix"} or if \code{type = "cor"}. Defaults to \code{"average"}
@@ -23,7 +23,7 @@
 #' @export
 #'
 
-plot.feature_calculations <- function(x, type = c("quality", "matrix", "cor", "violin"),
+plot.feature_calculations <- function(x, type = c("matrix", "cor", "violin", "box", "quality"),
                                       norm_method = c("zScore", "Sigmoid", "RobustSigmoid", "MinMax", "MaxAbs"),
                                       unit_int = FALSE,
                                       clust_method = c("average", "ward.D", "ward.D2", "single", "complete", "mcquitty", "median", "centroid"),
@@ -235,7 +235,7 @@ plot.feature_calculations <- function(x, type = c("quality", "matrix", "cor", "v
       p <- p +
         ggplot2::theme(axis.text = ggplot2::element_blank())
     }
-  } else{
+  } else if(type == "violin"){
 
     if(is.null(feature_names)){
       stop("feature_names argument must not be NULL if drawing a violin plot. Please enter a vector of valid feature names.")
@@ -256,6 +256,36 @@ plot.feature_calculations <- function(x, type = c("quality", "matrix", "cor", "v
       p <- p +
         ggplot2::geom_violin() +
         ggplot2::geom_point(position = ggplot2::position_jitter(w = 0.05), ...) +
+        ggplot2::labs(x = "Class",
+                      y = "Feature value") +
+        ggplot2::scale_colour_brewer(palette = "Dark2") +
+        ggplot2::theme_bw() +
+        ggplot2::theme(legend.position = "none",
+                       axis.text.x = ggplot2::element_text(angle = 90)) +
+        ggplot2::facet_wrap(~ names, scales = "free_y")
+    }
+  }
+
+  else{
+
+    if(is.null(feature_names)){
+      stop("feature_names argument must not be NULL if drawing a boxplot Please enter a vector of valid feature names.")
+    } else{
+      p <- x %>%
+        dplyr::filter(names %in% feature_names) %>%
+        dplyr::mutate(names = paste0(.data$feature_set, "_", .data$names))
+
+      if("group" %in% colnames(p)){
+        p <- p %>%
+          ggplot2::ggplot(ggplot2::aes(x = .data$group, y = .data$values, colour = .data$group))
+      } else{
+        p <- p %>%
+          dplyr::mutate(group = "Data") %>%
+          ggplot2::ggplot(ggplot2::aes(x = .data$group, y = .data$values))
+      }
+
+      p <- p +
+        ggplot2::geom_boxplot(...) +
         ggplot2::labs(x = "Class",
                       y = "Feature value") +
         ggplot2::scale_colour_brewer(palette = "Dark2") +
